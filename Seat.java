01d06a1ef4b3204f11899316c52abecbf487da1a
Sania G <150053740+saniagx@users.noreply.github.com>
2025-02-20 13:19:05 +0000

    Adding some attributes to Seat.java

--- Seat.java
+++ Seat.java
@@ -1,3 +1,13 @@
 //these are seat objects which can contain data attributes
 
+public class Seat {
+  private int seatNum;
+  private int rowNum; 
+  private Section seatSection;
+  
+  
+
+  
+
+}
 
