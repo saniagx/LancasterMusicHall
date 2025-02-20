a2fd2482f6356cb216d69c48dc20e3d0ac937f2c
neil <neil.daya@city.ac.uk>
2025-02-20 11:12:01 +0000

    drafted to match final requirements

--- IEvent.java
+++ IEvent.java
@@ -1,3 +1,56 @@
-public interface IEvent {
+import java.sql.Date;
+import java.sql.Time;
 
+public interface IEvent {
+    /** Attributes accessible through this interface:
+     *      eventID : int
+     *      eventName : String
+     *      eventType : String
+     *      eventDate : Date
+     *      eventStartTime : time
+     *      eventEndTime : time
+     *      eventPrice : float
+     *      venueID : int
+     *      venueName : String
+     *
+     *      I've omitted 'roomType' as eventType = 'meeting' does the same job
+     */
+
+    // No need for a meeting interface, meeting events can be implemented through this event interface
+
+    int getEventID();
+
+    void setEventID(int eventID);
+
+    String getEventName();
+
+    void setEventName(String eventName);
+
+    String getEventType();
+
+    void setEventType(String eventType);
+
+    Date getEventDate();
+
+    void setEventDate(Date eventDate);
+
+    Time getEventStartTime();
+
+    void setEventStartTime(Time eventStartTime);
+
+    Time getEventEndTime();
+
+    void setEventEndTime(Time eventEndTime);
+
+    float getEventPrice();
+
+    void setEventPrice(float eventPrice);
+
+    int getVenueID();
+
+    void setVenueID(int venueID);
+
+    String getVenueName();
+
+    void setVenueName(String venueName);
 }
