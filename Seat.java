fb0779dfda7f2c5e9bcc5f77f8e0474906ec8efc
Sania G <150053740+saniagx@users.noreply.github.com>
2025-02-20 13:40:42 +0000

    Attriubtes, getters and setters Seat.java

--- Seat.java
+++ Seat.java
@@ -1,13 +1,24 @@
 //these are seat objects which can contain data attributes
 
 public class Seat {
+
+  //attributes for each seat
   private int seatNum;
   private int rowNum; 
   private Section seatSection;
   
-  
+  //getters and setters for each seat
+  int getSeatNumber();
 
-  
+  void setSeatNumber(int seatNumber);
+
+  int getRowNumber();
+
+  void setRowNumber(int rowNumber);
+
+  string getSection();
+
+  void setSection(string section);
 
 }
 
