8d528313489011d8c272e6dfd8d1c8458f9ca28e
Sania G <150053740+saniagx@users.noreply.github.com>
2025-02-21 14:51:57 +0000

    Update Section.java

--- Section.java
+++ Section.java
@@ -1,1 +1,19 @@
 //these are section objects which can contain it's own 2D array
+
+public class Section {
+
+  //declare section name
+  //2D array to store the seats within the section
+  private string name;
+  Seat[][] seats = new Seats[row][col];
+
+
+  //getters and setters for the name and array of Seats
+  string getName();
+
+  void setName(string section);
+
+  seat[][] getSeats();
+
+  void addSeat(Seat seat); 
+}
