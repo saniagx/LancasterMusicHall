d582dfa2110852a096daf0b6d326cd7b9c719968
Sania G <150053740+saniagx@users.noreply.github.com>
2025-02-21 16:08:19 +0000

    Update ISeatingConfig.java

--- ISeatingConfig.java
+++ ISeatingConfig.java
@@ -1,11 +1,13 @@
 //this will contain section objects
 public interface ISeatingConfig{
 
+  
   //implemented inside Section
   String getSectionName();
 
   void setSectionName();
 
+  ArrayList<Seat> getSeats();
 
-
+  
 }
