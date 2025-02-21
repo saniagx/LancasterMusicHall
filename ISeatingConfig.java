aa71c41432d1ba864aae97013ac6fab02bede7de
Sania G <150053740+saniagx@users.noreply.github.com>
2025-02-21 16:08:41 +0000

    Update ISeatingConfig.java

--- ISeatingConfig.java
+++ ISeatingConfig.java
@@ -9,5 +9,25 @@ public interface ISeatingConfig{
 
   ArrayList<Seat> getSeats();
 
+   //getters and setters for each seat
+  int getSeatNumber();
+
+  void setSeatNumber(int seatNumber);
+
+  int getRowNumber();
+
+  void setRowNumber(int rowNumber);
+
+  string getSection();
+
+  void setSection(string section);
+
+  float getPrice();
+
+  void setPrice(float price);
+
+  boolean getBooked();
+
+  void setBooked(boolean booked);
   
 }
