63acffec13ec7709e7ef76f7579644a1b5f1b37f
Sania G <150053740+saniagx@users.noreply.github.com>
2025-02-21 16:06:18 +0000

    Update ISeatingConfig.java

--- ISeatingConfig.java
+++ ISeatingConfig.java
@@ -1,23 +1,11 @@
 //this will contain section objects
 public interface ISeatingConfig{
 
+  //implemented inside Section
+  String getSectionName();
 
-  
-  //this is to get the sections from the array of section objects
-  section getMainHall();
+  void setSectionName();
 
-  section getSmallHall();
 
-  section getRehearsalSpace();
 
-  //methods to get the 5 meeting rooms
-  section getMeetingA();
-
-  section getMeetingB();
-  
-  section getMeetingC();
-
-  section getMeetingD();
-
-  section getMeetingE();
 }
