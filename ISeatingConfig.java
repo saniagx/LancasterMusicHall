89e340088cf072da8ca5b96be9dc24fa207f6a90
Sania G <150053740+saniagx@users.noreply.github.com>
2025-02-21 16:09:17 +0000

    tidying ISeatingConfig.java

--- ISeatingConfig.java
+++ ISeatingConfig.java
@@ -4,30 +4,24 @@ public interface ISeatingConfig{
   
   //implemented inside Section
   String getSectionName();
-
   void setSectionName();
 
   ArrayList<Seat> getSeats();
 
    //getters and setters for each seat
   int getSeatNumber();
-
   void setSeatNumber(int seatNumber);
 
   int getRowNumber();
-
   void setRowNumber(int rowNumber);
 
   string getSection();
-
   void setSection(string section);
 
   float getPrice();
-
   void setPrice(float price);
 
   boolean getBooked();
-
   void setBooked(boolean booked);
   
 }
