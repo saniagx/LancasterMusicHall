3b1823143ae55cd27aa6057608068c20644cb6be
neil <neil.daya@city.ac.uk>
2025-02-22 18:36:38 +0000

    final calendar draft

--- ICalendar.java
+++ ICalendar.java
@@ -2,15 +2,36 @@ import java.sql.Date;
 import java.util.ArrayList;
 
 public interface ICalendar {
+
+    /**
+     * Adds event to the events ArrayList
+     * @param event Event object to be added
+     */
     void addEvent(IEvent event);
 
+    /**
+     * Removes event from the events ArrayList according to the given ID
+     * @param eventID Unique identifier for event objects
+     */
     void removeEvent(int eventID);
 
-    // All event attributes can be accessed via the event interfaces' methods
+    /**
+     * Returns event object matching the given ID
+     * @param eventID Unique identifier for event objects
+     * @return Event object or null if no Event in the ArrayList has the given ID
+     */
     IEvent getEvent(int eventID);
 
+    /**
+     * Returns the events ArrayList
+     * @return ArrayList containing all the events in the calendar
+     */
     ArrayList<IEvent> getAllEvents();
 
-    boolean getAvailability(Date date);
-    void setAvailability(boolean availability);
+    /**
+     * Returns true if the date given doesn't have an event booked on that date
+     * @param date SQL data type
+     * @return Boolean which is true if a date is available to be booked
+     */
+    boolean isAvailable(Date date);
 }
