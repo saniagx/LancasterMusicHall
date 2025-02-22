4821ed6fdba0b3cf6fb194acab7470276ebdbd92
mali3226 <159960677+mali3226@users.noreply.github.com>
2025-02-22 19:29:32 +0000

    Section class update
    Added constructor
    fields
    methods
    implementation of populating sections with seats

--- Section.java
+++ Section.java
@@ -8,6 +8,23 @@ public class Section {
   // Declare Section Name
   private String sectionName;
   private ArrayList<Seat> seats = new ArrayList<Seat>();;
+  private int numRows;
+  private int seatsPerRow;
+
+  // Constructor
+  public Section(String sectionName, int numRows, int seatsPerRow, float basePrice) {
+    this.sectionName = sectionName;
+    this.numRows = numRows;
+    this.seatsPerRow = seatsPerRow;
+    this.seats = new ArrayList<>();
+
+    // Generate seats
+    for (int row = 1; row <= numRows; row++) {
+      for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
+        seats.add(new Seat(seatNum, row, this, basePrice));
+      }
+    }
+  }
 
   // Constructor
   public Section(String sectionName){
@@ -28,14 +45,26 @@ public class Section {
     return seats;
   }
 
-  // Add a seat to the section
+  // Adding seat manually
   public void addSeat(Seat seat) {
-    this.seats.add(seat);
+    if (!seats.contains(seat)) {
+      seats.add(seat);
+    }
   }
 
-  // Remove a seat from the section
+  // Remove a seat manually
   public void removeSeat(Seat seat) {
-    this.seats.remove(seat);
+    seats.remove(seat);
+  }
+
+  // Getter for Number of Rows
+  public int getNumRows() {
+    return numRows;
+  }
+
+  // Getter for Seats Per Row
+  public int getSeatsPerRow() {
+    return seatsPerRow;
   }
 
 
