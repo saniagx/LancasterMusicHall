013cbf50cda69c8423fefc550b8e143cec43aa70
Nadwaali <159057594+Nadwaali@users.noreply.github.com>
2025-02-25 13:15:10 +0000

    Update DatabaseConnection.java

---
+++ DatabaseConnection.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+public class DatabaseConnection {
+    public static void main(String[] args) {
+        // Database Details
+        String host = "smcse-stuproj00.city.ac.uk";
+        String port = "3306";  //MySQL port
+        String dbName = "<in2033t02>"; 
+        String user = "<in2033t02_a>"; 
+        String password = "<wceWZykvyss>"; 
+
+        // Connection URL
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName + "?user=" + user + "&password=" + password;
+
+        try {
+            //connection to the database
+            Connection conn = DriverManager.getConnection(url);
+            System.out.println("Connected to the database!");
+
+            // Our database operations can go here 
+
+            // Clsoing connection when done 
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
