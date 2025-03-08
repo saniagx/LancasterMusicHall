d8541fecbf295f2533d0e90bca0acfeda604f1df
neil <neil.daya@city.ac.uk>
2025-03-08 13:43:17 +0000

    login screen implemented

---
+++ gui/Main.java
@@ -0,0 +1,33 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+            Scene scene = new Scene(root, 800, 600);
+
+            Image LMHlogo = new Image("assets/LMHlogo.png");
+            stage.getIcons().add(LMHlogo);
+
+            stage.setTitle("Lancaster's Music Hall");
+            stage.setResizable(false);
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
